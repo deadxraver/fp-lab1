a89b7d605e476c038b6880615aeaffb15a1a0206
deadxraver <deadxraver@yandex.ru>
2025-10-27 23:05:19 +0300

    format code

--- src/12/Main.hs
+++ src/12/Main.hs
@@ -1,6 +1,7 @@
 import System.Environment (getArgs)
 
 defaultArg = 500
+
 parseArgs :: [String] -> Int
 parseArgs clArgs = if not (null clArgs) then read (head clArgs) :: Int else defaultArg
 
@@ -8,26 +9,27 @@ triangleNumber :: (Integral a) => a -> a
 triangleNumber n = n * (n + 1) `div` 2
 
 -- map + inf list
-triangleNumbers :: Integral a => [a]
-triangleNumbers = map triangleNumber [0..]
+triangleNumbers :: (Integral a) => [a]
+triangleNumbers = map triangleNumber [0 ..]
 
 countDivs :: (Integral a) => a -> [a] -> a
 -- rec
 countDivs n arr
-              | null arr = 0
-              | x * x > n = 0
-              | x * x == n = 1
-              | n `mod` x == 0 = 2 + countDivs n xs
-              | otherwise = countDivs n xs
-              where (x:xs) = arr
+  | null arr = 0
+  | x * x > n = 0
+  | x * x == n = 1
+  | n `mod` x == 0 = 2 + countDivs n xs
+  | otherwise = countDivs n xs
+  where
+    (x : xs) = arr
 
 -- filter + inf list again
 highlyDivisibleTriangleNumbers :: (Integral a) => a -> [a]
 highlyDivisibleTriangleNumbers divs =
-                    filter (\n -> countDivs n [1..] >= divs) triangleNumbers
+  filter (\n -> countDivs n [1 ..] >= divs) triangleNumbers
 
 main :: IO ()
 -- test output
 main = do
-      args <- getArgs
-      print (head (highlyDivisibleTriangleNumbers (parseArgs args)))
+  args <- getArgs
+  print (head (highlyDivisibleTriangleNumbers (parseArgs args)))
