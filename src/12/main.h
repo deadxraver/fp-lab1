b197b8911e1c18abef32e48eacecbb3f195e30f4
deadxraver <deadxraver@yandex.ru>
2025-10-27 12:16:50 +0300

    include guards planet

--- src/12/main.h
+++ src/12/main.h
@@ -1,3 +1,7 @@
+#ifndef SRC_12_MAIN_H
+
+#define SRC_12_MAIN_H
+
 #include <stdio.h>
 
 #define DEFAULT_ARG 500 // в задаче написано а найдтие первое число с 500 делителей поэтому дефолтное значение такое
@@ -7,3 +11,5 @@ void exit(int); // джентльменский сискол
 int count_divs(int); // cxbnftn ltkbntkb
 
 int number_with_divs(int); // ищет первое число с заданным количеством делетелев
+
+#endif // !SRC_12_MAIN_H
