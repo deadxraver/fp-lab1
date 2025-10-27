fc8335f4d755e17b65b6405685a9ede43b628eb1
deadxraver <deadxraver@yandex.ru>
2025-10-27 17:29:15 +0300

    c solution for 2nd problem

--- Makefile
+++ Makefile
@@ -9,12 +9,16 @@ SRC=src
 
 CC=gcc
 CSRC1=$(SRC)/$(PROBLEM1)/*.c
+CSRC2=$(SRC)/$(PROBLEM2)/*.c
 
 HSC=ghc
 HSSRC1=$(SRC)/$(PROBLEM1)/*.hs
+HSSRC2=$(SRC)/$(PROBLEM2)/*.hs
 
 CEXEC1=c-$(PROBLEM1)
+CEXEC2=c-$(PROBLEM2)
 HSEXEC1=hs-$(PROBLEM1)
+HSEXEC2=hs-$(PROBLEM2)
 
 all: lint build test
 	@echo 'build & test successfully finished'
@@ -26,17 +30,18 @@ build: build-c build-hs
 	@echo 'build finished, put binaries to $(BUILD)/'
 
 build-hs: $(HSSRC1)
-	$(HSC) $^ -o $(BUILD)/$(HSEXEC1)
+	$(HSC) $(HSSRC1) -o $(BUILD)/$(HSEXEC1)
 
-build-c: $(CSRC1)
+build-c: $(CSRC1) $(CSRC2)
 	-mkdir $(BUILD)
-	$(CC) -o $(BUILD)/$(CEXEC1) $^
+	$(CC) -o $(BUILD)/$(CEXEC1) $(CSRC1)
+	$(CC) -o $(BUILD)/$(CEXEC2) $(CSRC2)
 
 test: test-c test-hs
 	@echo 'Tests OK'
 
 test-hs:	build-hs
-	@echo "Running hs tests..."
+	@echo "Running hs tests for problem $(PROBLEM1)..."
 	[ $$($(BUILD)/$(HSEXEC1)) = 76576500 ];
 	@echo OK
 	[ $$($(BUILD)/$(HSEXEC1) 0) = 0 ];
@@ -45,12 +50,15 @@ test-hs:	build-hs
 	@echo OK
 
 test-c:	build-c
-	@echo "Running c tests..."
+	@echo "Running c tests for problem $(PROBLEM1)..."
 	[ $$($(BUILD)/$(CEXEC1)) = 76576500 ];
 	@echo OK
 	[ $$($(BUILD)/$(CEXEC1) 0) = 0 ];
 	@echo OK
 	[ $$($(BUILD)/$(CEXEC1) 6) = 28 ];
+	@echo OK
+	@echo "...and for problem $(PROBLEM2)..."
+	[ $$($(BUILD)/$(CEXEC2)) = 23 ];
 	@echo OK
 
 clean:
--- src/18/main.c
+++ src/18/main.c
@@ -1,5 +1,53 @@
 #include "main.h"
 
+struct node* create_node(int num) {
+  struct node* node = (struct node*) malloc(sizeof(struct node));
+  node->number = num;
+  node->left = NULL;
+  node->right = NULL;
+  return node;
+}
+
+struct node* generate_default() {
+  struct node* node = create_node(3);
+
+  node->left = create_node(7);
+  node->right = create_node(4);
+
+  node->left->left = create_node(2);
+  node->left->right = create_node(4);
+  node->right->left = node->left->right;
+  node->right->right = create_node(6);
+
+  node->left->left->left = create_node(8);
+  node->left->left->right = create_node(5);
+  node->left->right->left = node->left->left->right;
+  node->left->right->right = create_node(9);
+  node->right->left->left = node->left->right->left;
+  node->right->left->right = node->left->right->right;
+  node->right->right->left = node->right->left->right;
+  node->right->right->right = create_node(3);
+  return node;
+}
+
+static void free_left_row(struct node* node) {
+  if (node) {
+    free_left_row(node->left);
+    node->left = NULL;
+    free(node);
+  }
+}
+
+void free_node(struct node* node) {
+  if (node == NULL)
+    return;
+  struct node* next_node;
+  for (node; node; node = next_node) {
+    next_node = node->right;
+    free_left_row(node);
+  }
+}
+
 static inline int max(int a, int b) {
   return a > b ? a : b;
 }
@@ -17,5 +65,9 @@ int max_path(struct node* node) {
 }
 
 int main(int argc, char* argv[]) {
+  struct node* init_node = generate_default();
+  printf("%d\n", max_path(init_node)); // should be 23
+  free_node(init_node);
+  init_node = NULL;
   return 0;
 }
